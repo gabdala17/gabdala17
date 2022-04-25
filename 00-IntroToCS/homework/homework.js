028b5879a5561f962cfa29dec451cc073d39e769
antoabdala <gisella.abdala@gmail.com>
2022-04-25 18:15:06 -0300

    first commit

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,12 +2,46 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-  // algo
+  let nuevoNumero = num.split("");
+  let suma = 0;
+
+  for (let i = 0; i < nuevoNumero.length; i++) {
+     suma += +nuevoNumero[i] * 2 ** (nuevoNumero.length - 1 - i);
+  }
+  return (suma) ;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  var array = [];
+  do{
+    if(num === 0){
+      return 0;
+    }
+    
+    else if(num % 2 !== 0){
+      array.push(1)
+    }
+    else if(num % 2 === 0){
+      array.push(0)
+    }
+    num = Math.floor(num/2);
+  }
+
+
+  while(num >= 1);
+  console.log(array)
+  array.reverse();
+  console.log(array)
+  var binario = array.join("")
+  return binario
 }
+
+// No se pueden usar: 
+// parseInt
+// toString
+
+
 
 module.exports = {
   BinarioADecimal,
